d96b77e63a0feacf5dc83157bb0eb75b3d7cfa4e
Cesar Millan <cesarmillan@MacBook-Pro-de-Cesar.local>
2019-11-14 09:40:14 -0600

    nothing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>no es preciso actualizar</h1>
+    <button v-on:click="saludar()"> SALUDAR!</button>
   </div>
 </template>
+
+<style lang="css" scoped>
+  H1{
+    color: red;
+  }
+</style>
+
+
+<script>
+export default {
+  name: 'about-component',
+
+  methods: {
+    saludar(){
+      alert('hola ')
+    }
+  },
+  data: () => {
+    return {
+      var1: 4567,
+      name: 'ernesto'
+    }
+  }
+}
+</script>
